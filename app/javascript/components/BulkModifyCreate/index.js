565ca9496024729e811ec574df246d383a8bea98
Jessi Shank <jessishank1@gmail.com>
2020-11-24 15:48:35 -0800

    formatting

--- app/javascript/components/BulkModifyCreate/index.js
+++ app/javascript/components/BulkModifyCreate/index.js
@@ -1,71 +1,64 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import { 
-    Button, 
-    Typography,
-    Grid,
-} from '@material-ui/core';
+import { Button, Typography, Grid } from '@material-ui/core';
 import { DataGrid } from '@material-ui/data-grid';
 import styles from './styles';
 
-function BulkModifyCreate
-({
-    classes,
-    actionItemId,
-    assignments,
-    assignmentsList,
-    actionItem,
+function BulkModifyCreate({
+  classes,
+  actionItemId,
+  assignments,
+  assignmentsList,
+  actionItem,
 }) {
-    console.log(assignmentsList)
-    const columns = [
-        { field: 'participant_id', headerName: 'ID', width: 70 },
-        { field: 'participants', headerName: 'Name', width: 130 },
-        {
-            field: 'updated_at',
-            headerName: 'Assigned Date',
-            type: 'date',
-            width: 150,
-            format: (value) => formatDate(value),
-        },
-        {
-            field: 'due_date',
-            headerName: 'Due Date',
-            type: 'date',
-            width: 150,
-            format: (value) => value.formatDate(value),
-        },
-        {
-            field: 'completed_participant',
-            headerName: 'Completed',
-            width: 130,
-        },
-    ];
-    return (
-        <>
-        <Grid
-            container
-            justify="center"
-        >
-            <Grid
-                item
-            >
-                <Typography variant="h4">{actionItem.title}</Typography>
-                <Typography variant="h6">{actionItem.description}</Typography>
-            </Grid>
-            
+  console.log(assignmentsList);
+  const columns = [
+    { field: 'participant_id', headerName: 'ID', width: 70 },
+    { field: 'participants', headerName: 'Name', width: 130 },
+    {
+      field: 'updated_at',
+      headerName: 'Assigned Date',
+      type: 'date',
+      width: 150,
+      format: value => formatDate(value),
+    },
+    {
+      field: 'due_date',
+      headerName: 'Due Date',
+      type: 'date',
+      width: 150,
+      format: value => value.formatDate(value),
+    },
+    {
+      field: 'completed_participant',
+      headerName: 'Completed',
+      width: 130,
+    },
+  ];
+  return (
+    <>
+      <Grid container justify="center">
+        <Grid item>
+          <Typography variant="h4">{actionItem.title}</Typography>
+          <Typography variant="h6">{actionItem.description}</Typography>
         </Grid>
-        <DataGrid rows={assignments} columns={columns} pageSize={10} checkboxSelection />
-        </>
-    )
-};
+      </Grid>
+      <DataGrid
+        rows={assignments}
+        columns={columns}
+        pageSize={10}
+        checkboxSelection
+      />
+    </>
+  );
+}
 
 BulkModifyCreate.propTypes = {
-    classes: PropTypes.object.isRequired,
-    actionItemId: PropTypes.string.isRequired,
-    actionItem: PropTypes.object.isRequired,
-    assignments: PropTypes.array.isRequired,
-
+  classes: PropTypes.object.isRequired,
+  actionItemId: PropTypes.string.isRequired,
+  actionItem: PropTypes.object.isRequired,
+  assignments: PropTypes.array.isRequired,
 };
 
 export default withStyles(styles)(BulkModifyCreate);
--- app/javascript/components/BulkModifySelectCard/index.js
+++ app/javascript/components/BulkModifySelectCard/index.js
@@ -9,38 +9,38 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './styles';
 
 function BulkModifySelectCard({ classes, row }) {
-    const showActionItem = () => {
-        const actionItemId = row.id;
-        window.location.assign(`/assignments/bulk-modify/${actionItemId}`);
-    }
-    return (
-        <>
-            <TableCell>{row.id}</TableCell>
-            <TableCell
-                style={{ cursor: 'pointer'}}
-                onClick={showActionItem}
-                onKeyDown={showActionItem}
-            >
-                {row.title}
-            </TableCell>
-            <TableCell>{formatDate(row.updated_at)}</TableCell>
-            <TableCell>{row.category}</TableCell>
-            <TableCell align="center">
-                <FontAwesomeIcon
-                onClick={showActionItem}
-                icon={faChevronRight}
-                color="grey"
-                style={{ cursor: 'pointer' }}
-                className={classes.iconLarge}
-                />
-            </TableCell>
-        </>
-    )
-};
+  const showActionItem = () => {
+    const actionItemId = row.id;
+    window.location.assign(`/assignments/bulk-modify/${actionItemId}`);
+  };
+  return (
+    <>
+      <TableCell>{row.id}</TableCell>
+      <TableCell
+        style={{ cursor: 'pointer' }}
+        onClick={showActionItem}
+        onKeyDown={showActionItem}
+      >
+        {row.title}
+      </TableCell>
+      <TableCell>{formatDate(row.updated_at)}</TableCell>
+      <TableCell>{row.category}</TableCell>
+      <TableCell align="center">
+        <FontAwesomeIcon
+          onClick={showActionItem}
+          icon={faChevronRight}
+          color="grey"
+          style={{ cursor: 'pointer' }}
+          className={classes.iconLarge}
+        />
+      </TableCell>
+    </>
+  );
+}
 
 BulkModifySelectCard.propTypes = {
-    classes: PropTypes.object.isRequired,
-    row: PropTypes.object,
+  classes: PropTypes.object.isRequired,
+  row: PropTypes.object,
 };
 
 export default withStyles(styles)(BulkModifySelectCard);
