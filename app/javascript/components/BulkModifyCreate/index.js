63b8b9bcbeb7de3f15e491ddee0ab53a8049c796
becky <beckymccarthy777@gmail.com>
2020-12-03 11:58:48 -0800

    Created BulkModifyModal from BulkModifyCreate
    index.js and styles.js allowing modification of title,
    description and due date. Also loads all participants
    ever assigned the action item.

--- app/javascript/components/BulkModifyCreate/index.js
+++ app/javascript/components/BulkModifyCreate/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import { Button, Typography, Grid } from '@material-ui/core';
 import formatDate from 'utils/utils';
 import { DataGrid } from '@material-ui/data-grid';
 import styles from './styles';
+import BulkModifyModal from '../BulkModifyModal';
+import MUIRichTextEditor from 'mui-rte';
 
 function BulkModifyCreate({
   classes,
@@ -12,9 +14,9 @@ function BulkModifyCreate({
   assignmentsList,
   actionItem,
 }) {
-    console.log('actionItem', actionItem);
-    console.log('actionItemId', actionItemId);
-    console.log('assignmentsList', assignmentsList);
+    const [selections, setSelections] = useState([]);
+    const [open, setOpen] = useState(false);
+    const [selectedAssignments, setSelectedAssignments] = useState([]);
 
     const dateHelper = {
         type: 'date',
@@ -22,6 +24,20 @@ function BulkModifyCreate({
         valueFormatter: ({ value }) => formatDate(value),
     };
 
+    const handleOpenBulkModifyModal = () => {
+        setSelectedAssignments(assignmentsList.filter(assignment => {
+            return selections.includes(assignment.id.toString());
+        }));
+        setOpen(true);
+    };
+
+    // const handleClose () => {
+    //     setOpen(false);
+    // };
+   
+    console.log('assignmentsList', assignmentsList);
+    
+  
     return (
         <>
             <Grid
@@ -32,21 +48,47 @@ function BulkModifyCreate({
                     item
                 >
                     <Typography variant="h4">{actionItem.title}</Typography>
-                    <Typography variant="h6">{actionItem.description}</Typography>
+                    {actionItem.description[0] === '{' ? 
+                    (<MUIRichTextEditor value={actionItem.description} readOnly toolbar={false} />) :
+                    (<Typography variant="h6">{actionItem.description}</Typography>)}
+                </Grid>
+                <Grid container item justify="flex-end">
+                    <Button
+                        // className={this.props.classes.primaryButton}
+                        style={{ margin: '0 30 20 0' }}
+                        variant="contained"
+                        color="primary"
+                        onClick={handleOpenBulkModifyModal}
+                    >
+                        MODIFY
+                    </Button>
+                    <BulkModifyModal
+                        open={open} 
+                        title={actionItem.title}
+                        description={actionItem.description} 
+                        selectedAssignments={selectedAssignments}
+                        // onClose={handleClose}                   
+                    />
                 </Grid>
                 
             </Grid>
-                <DataGrid
-                    columns={[
-                        { field: 'participant', headerName: 'Name', width: 160, valueFormatter: ({value}) =>  value.name },
-                        { field: 'updated_at', headerName: 'Assigned Date', ...dateHelper },
-                        { field: 'due_date', headerName: 'Due Date', ...dateHelper },
-                        { field: 'completed_participant', headerName: 'Completed', width: 130 },
-                    ]}
-                    rows={assignmentsList}
-                    pageSize={10} 
-                    checkboxSelection 
-                />
+            <DataGrid
+                columns={[
+                    { field: 'participant', headerName: 'Name', width: 160, valueFormatter: ({value}) =>  value.name },
+                    { field: 'updated_at', headerName: 'Assigned Date', ...dateHelper },
+                    { field: 'due_date', headerName: 'Due Date', ...dateHelper },
+                    { field: 'completed_participant', headerName: 'Completed', width: 130 },
+
+                ]}
+                rows={assignmentsList}
+                pageSize={10} 
+                checkboxSelection
+                onSelectionChange={(newSelection) => {
+                    console.log('newSelection', newSelection);
+                    setSelections(newSelection.rowIds);
+                  }}
+            />
+            
         </>
     )
 };
@@ -55,7 +97,6 @@ BulkModifyCreate.propTypes = {
   classes: PropTypes.object.isRequired,
   actionItemId: PropTypes.string.isRequired,
   actionItem: PropTypes.object.isRequired,
-  assignments: PropTypes.array.isRequired,
   assignmentsList: PropTypes.array.isRequired,
 };
 
